77106802c801cd25c19f800b6d012b12048ac4e5
Snehakumari06 <snehasingh5754@gmail.com>
2025-06-17 10:32:26 +0530

    update6

--- javascriptlogin/script.js
+++ javascriptlogin/script.js
@@ -1,6 +1,8 @@
 const loginForm = () => {
     const str = `<div>
     <h3>Login Form</h3>
+    <p>Username: <input type="text" id="username" placeholder="Enter your username"></p>
+    <p>Password: <input type="password" id="password" placeholder="Enter your password"></p>
     <p><button onclick = 'showHome()'>Submit</button></p>
     <p><button onclick = 'registerForm()'>Create Account</button></p>
     </div>
@@ -12,6 +14,9 @@ const loginForm = () => {
 const registerForm = () => {
     const str = `<div>
     <h3>Register Form</h3>
+    <p>Username: <input type="text" id="newUsername" placeholder="Enter your username"></p>
+    <p>Email: <input type="email" id="email" placeholder="Enter your email"></p>
+    <p>Password: <input type="password" id="newPassword" placeholder="Enter your password"></p>
     <p><button onclick = 'showHome()'>Submit</button></p>
     <p><button onclick ='loginForm()'>Already a member? Login..   </button></p>
     `
