418a4df31b184b90d949de13cd749ae738e1ea71
Snehakumari06 <snehasingh5754@gmail.com>
2025-06-17 09:42:23 +0530

    update4

---
+++ p1/script.js
@@ -0,0 +1,25 @@
+document.getElementById('createAccountLink').addEventListener('click', function () {
+    document.getElementById('loginBox').style.display = 'none';
+    document.getElementById('createAccountBox').style.display = 'block';
+});
+
+document.getElementById('loginLink').addEventListener('click', function () {
+    document.getElementById('createAccountBox').style.display = 'none';
+    document.getElementById('loginBox').style.display = 'block';
+});
+
+document.getElementById('createAccountForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    document.getElementById('welcomeMessage').textContent = `Hey ${name}`;
+    document.getElementById('createAccountBox').style.display = 'none';
+    document.getElementById('welcomeBox').style.display = 'block';
+});
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const email = document.getElementById('loginEmail').value;
+    document.getElementById('welcomeMessage').textContent = `Hey ${email}`;
+    document.getElementById('loginBox').style.display = 'none';
+    document.getElementById('welcomeBox').style.display = 'block';
+});
