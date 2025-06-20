d86d1f75c61e75d541f7df053bb7a0c246f4c35d
Snehakumari06 <snehasingh5754@gmail.com>
2025-06-20 09:59:12 +0530

    update

--- javascript/file1.js
+++ javascript/file1.js
@@ -1,1 +1,26 @@
+//Variables and their types and their conversion
+
 console.log("hello world");
+let a=10
+let b="sneha"
+let c=true
+let d 
+let e=null
+console.log(a);                       //value
+console.log(typeof(b));               //type
+console.log(typeof(d));               //undefined
+console.log(typeof(e));              //object
+
+let strAge = String(a);              //converting number to string
+console.log(typeof strAge);
+
+let rating = "5";
+console.log(typeof rating);         //string
+letstrRating = Number(rating);      //converting string to number
+console.log(typeof letstrRating);   //number
+
+let flag = "true"
+console.log(typeof flag);           //string
+let boolFlag = Boolean(flag);       //converting string to boolean
+console.log(typeof boolFlag);      //boolean
+
---
+++ javascript/file2.js
@@ -0,0 +1,37 @@
+//Operators
+
+// let a = 56
+// let b = 10
+// console.log(a+b);
+// console.log(a%b);
+
+// let c = "10"
+// let d = 10
+// console.log(c+d);
+// console.log(c*d);   //add string ko bhi kr skte hai but mul bs no ko
+
+// let e="2"
+// let f=2
+// console.log(e==f);  //true
+
+// let e="2"
+// let f=2
+// console.log(e===f);   //false, type bhi check krta hai
+
+// let g=10
+// let h=2
+// console.log(g!==h);  //true, not equal to
+
+// let i = 10
+// let j = 2
+// let k = 12
+// console.log(i>j && i>k);
+
+// let a= 10
+// let b=2
+// console.log(!a>b);
+
+// let a=10
+// let b=2
+// console.log(a**b);
+
